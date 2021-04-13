35dcd48f961680b8d1ef34ecf0ed040528710932
moskomule <hataya@nlab-mpg.jp>
2021-04-13 20:08:42 +0900

    fix

--- vit.py
+++ vit.py
@@ -5,7 +5,7 @@ from homura import lr_scheduler, reporters
 from homura.modules import SmoothedCrossEntropy
 from homura.trainers import SupervisedTrainer
 from homura.vision.data import DATASET_REGISTRY
-from torchvision.transforms import AutoAugment
+from torchvision.transforms import AutoAugment, RandomErasing
 
 from models.vit import ViTEMA, ViTs
 from utils import distributed_ready_main
@@ -23,7 +23,8 @@ class ViTTraner(SupervisedTrainer):
             {"params": params_dict['decay'], "weight_decay": self.optim_cfg.weight_decay},
             {"params": params_dict['no_decay'], "weight_decay": 0}
         ]
-        self.optimizer = torch.optim._multi_tensor.AdamW(optim_groups, lr=self.optim_cfg.lr,
+        self.optimizer = torch.optim._multi_tensor.AdamW(optim_groups,
+                                                         lr=self.optim_cfg.lr,
                                                          weight_decay=self.optim_cfg.weight_decay)
         self.logger.debug(self.optimizer)
 
@@ -31,11 +32,14 @@ class ViTTraner(SupervisedTrainer):
 @chika.config
 class DataConfig:
     batch_size: int = 128
+    autoaugment: bool = False
+    random_erasing: bool = False
 
 
 @chika.config
 class ModelConfig:
     name: str = chika.choices(*ViTs.choices())
+    droppath_rate: float = 0.1
     ema: bool = False
 
 
@@ -43,6 +47,7 @@ class ModelConfig:
 class OptimConfig:
     lr: float = 5e-4
     weight_decay: float = 0.05
+    label_smoothing: float = 0.1
     epochs: int = 200
 
 
@@ -55,6 +60,9 @@ class Config:
     debug: bool = False
     amp: bool = False
 
+    def __post_init__(self):
+        self.optim.lr *= self.data.batch_size * homura.get_world_size() / 512
+
 
 @chika.main(cfg_cls=Config)
 @distributed_ready_main
@@ -63,20 +71,22 @@ def main(cfg: Config):
         import rich
         rich.print(cfg)
     vs = DATASET_REGISTRY("imagenet")
-    aa = vs.default_train_da.copy() + [AutoAugment()]
+    train_da = vs.default_train_da.copy() + [AutoAugment()] if cfg.data.autoaugment else None
+    post_da = [RandomErasing()] if cfg.data.random_erasing else None
     train_loader, test_loader = vs(batch_size=cfg.data.batch_size,
-                                   train_da=aa,
+                                   train_da=train_da,
+                                   post_norm_train_da=post_da,
                                    train_size=cfg.data.batch_size * 50 if cfg.debug else None,
                                    test_size=cfg.data.batch_size * 50 if cfg.debug else None,
                                    num_workers=8)
-    model = ViTs(cfg.model.name)()
+    model = ViTs(cfg.model.name)(droppath_rate=cfg.model.droppath_rate)
     if cfg.model.ema:
         model = ViTEMA(model, 0.99996)
     scheduler = lr_scheduler.CosineAnnealingWithWarmup(cfg.optim.epochs, 1, 5)
 
     with ViTTraner(model,
                    None,
-                   SmoothedCrossEntropy(),
+                   SmoothedCrossEntropy(cfg.optim.label_smoothing),
                    reporters=[reporters.TensorboardReporter(".")],
                    scheduler=scheduler,
                    use_amp=cfg.amp,
@@ -84,7 +94,7 @@ def main(cfg: Config):
                    report_accuracy_topk=5,
                    optim_cfg=cfg.optim
                    ) as trainer:
-        for epoch in trainer.epoch_range(cfg.optim.epochs):
+        for _ in trainer.epoch_range(cfg.optim.epochs):
             trainer.train(train_loader)
             trainer.test(test_loader)
             trainer.scheduler.step()
