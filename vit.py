c03f43d0baa72063070275cca2b1a3337489857b
moskomule <hataya>
2021-11-24 20:02:56 +0100

    update homura

--- vit.py
+++ vit.py
@@ -11,18 +11,6 @@ from models.vit import ViTEMA, ViTs
 from vision_utils import fast_collate, gen_mix_collate
 
 
-@torch.no_grad()
-def accuracy(input: torch.Tensor,
-             target: torch.Tensor,
-             top_k: int = 1
-             ) -> torch.Tensor:
-    if target.ndim == 2:
-        target = target.argmax(dim=1)
-    pred_idx = input.argmax(dim=-1, keepdim=True) if top_k == 1 else input.topk(k=top_k, dim=-1).indices
-    target = target.view(-1, 1).expand_as(pred_idx)
-    return (pred_idx == target).float().sum(dim=1).mean()
-
-
 class ViTTraner(SupervisedTrainer):
     def __init__(self, *args, **kwargs):
         self.optim_cfg = kwargs.pop('optim_cfg')
@@ -39,35 +27,6 @@ class ViTTraner(SupervisedTrainer):
                                                          lr=self.optim_cfg.lr,
                                                          weight_decay=self.optim_cfg.weight_decay)
         self.logger.debug(self.optimizer)
-
-    def iteration(self,
-                  data: tuple[torch.Tensor, torch.Tensor]
-                  ) -> None:
-        # will be removed once new accuracy is merged into homura
-        input, target = data
-        with torch.cuda.amp.autocast(self._use_amp):
-            output = self.model(input)
-            loss = self.loss_f(output, target)
-
-        if self.is_train:
-            self.optimizer.zero_grad()
-            if self._use_amp:
-                self.scaler.scale(loss).backward()
-                self.scaler.step(self.optimizer)
-                self.scaler.update()
-            else:
-                loss.backward()
-                self.optimizer.step()
-            if self.update_scheduler_iter:
-                self.scheduler.step()
-        if self._is_debug and torch.isnan(loss):
-            self.logger.warning("loss is NaN")
-
-        self.reporter.add('accuracy', accuracy(output, target))
-        self.reporter.add('loss', loss.detach_())
-        if self._report_topk is not None:
-            for top_k in self._report_topk:
-                self.reporter.add(f'accuracy@{top_k}', accuracy(output, target, top_k))
 
 
 @chika.config
@@ -128,6 +87,7 @@ def main(cfg: Config):
     vs.collate_fn = fast_collate if cfg.data.mixup + cfg.data.cutmix == 0 else gen_mix_collate(vs.num_classes,
                                                                                                cfg.data.mixup,
                                                                                                cfg.data.cutmix)
+    vs.test_collate_fn = fast_collate
     model = ViTs(cfg.model.name)(droppath_rate=cfg.model.droppath_rate, dropout_rate=cfg.model.dropout_rate)
     train_da = vs.default_train_da.copy()
     test_da = vs.default_test_da.copy()
